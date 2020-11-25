6d661031678f135d6767400f76b0795e1f838389
Rafael Stoner <rafaelstoner7@gmail.com>
2020-11-25 22:24:21 +0200

    Terminada a validação dos campos com js

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,16 +5,56 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
+
+
+
+// Armazenar dados do formulário 
+
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        password: form.password.value
+    };
+    
+    // Acção a realizar com os dados 
+    console.log(user);
+}
 
 // Verificar se o form é válido
 function validateForm() {
     //Usando API Constraints
     isValid = form.checkValidity();
+
+    // Dar estilo se o form for inválido
     if (!isValid) {
         message.textContent = "Por favor preencha todos campos";
         message.style.color = 'red';
         messageContainer.style.borderColor = 'red';
+        return;
     }
+    if (passord1El.value === passord2El.value) {
+        passwordsMatch = true;
+        passord1El.style.borderColor = 'green';
+        passord2El.style.borderColor = 'green';
+    } else {
+        passwordsMatch = false;
+        message.textContent = "As senhas não combinam";
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        passord1El.style.borderColor = 'red';
+        passord2El.style.borderColor = 'red';
+        return;
+    }
+    
+    if (isValid && passwordsMatch) {
+        message.textContent = "Registado com Sucesso!";
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+    }
+
 
 
 
@@ -24,6 +64,11 @@ function validateForm() {
 function processFormData(e) {
     e.preventDefault();
     validateForm();
+    
+    // Submeter os dados se válidos
+    if(isValid && passwordsMatch) {
+        storeFormData();
+    }
 
 
 }
